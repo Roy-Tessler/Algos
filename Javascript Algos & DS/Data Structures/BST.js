bbd00db37ad53e76bb4a6ac1a9df05f7019cc907
Roy-Tessler <23tessler@gmail.com>
2020-08-01 14:04:41 +0300

    Adding DFS & BFS implementation

--- Javascript Algos & DS/Data Structures/BST.js
+++ Javascript Algos & DS/Data Structures/BST.js
@@ -53,4 +53,50 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+  BFS() {
+    let node = this.root;
+    let data = [];
+    let queue = [];
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  DFSPreOrder() {
+    let data = [];
+    let current = this.root;
+    const traverse = node => {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+    traverse(current);
+    return data;
+  }
+  DFSPostOrder() {
+    let data = [];
+    let current = this.root;
+    const traverse = node => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    };
+    traverse(current);
+    return data;
+  }
+  DFSInOrder() {
+    let data = [];
+    let current = this.root;
+    const traverse = node => {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    };
+    traverse(current);
+    return data;
+  }
 }
