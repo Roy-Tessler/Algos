96183f3b543996d96c546d08c8269235170cbdc0
Roy-Tessler <23tessler@gmail.com>
2020-08-09 14:16:18 +0300

    adding part 2 finishing touches

--- Wix/public.js
+++ Wix/public.js
@@ -15,13 +15,54 @@ function getData() {
     });
 }
 
+async function getNotificationData() {
+  let messagesQuery = await wixData
+    .query("Messages")
+    .limit(1000)
+    .find();
+  let followingQuery = await wixData
+    .query("following")
+    .eq("_id", wixUsers.currentUser.id)
+    .find();
+
+  if (!followingQuery.items[0]) return;
+  followingQuery = followingQuery.items[0].follow;
+  let filteredQuery = messagesQuery.items.filter(msg =>
+    followingQuery.includes(msg.Email)
+  );
+  return filteredQuery;
+}
+
 $w.onReady(() => {
   $w("#messagesWrite").onAfterSave(async () => {
     let data = await getData();
     $w("#messagesList").data = data;
+    let ownerEmail = data[0].Email;
+    notify(ownerEmail);
+  });
+  $w("#notifications").onAfterSave(async () => {
+    let notData = await getNotificationData();
+    if (!Array.isArray(notData)) {
+      notData = [];
+    }
+    $w("#notificationsRead").data = notData;
   });
 });
 
+async function notify(ownerEmail) {
+  let user = wixUsers.currentUser;
+  console.log("current user", wixUsers.currentUser);
+  let followRow = await wixData
+    .query("following")
+    .eq("_id", user.id)
+    .find();
+  let amFollowing = followRow.items[0].follow ? followRow.items[0].follow : [];
+
+  if (amFollowing.includes(ownerEmail)) {
+    console.log("showing badge");
+    showBadge();
+  }
+}
 $w.onReady(function() {
   wixData
     .query("Members/PrivateMembersData")
@@ -40,6 +81,13 @@ $w.onReady(function() {
 });
 
 export async function followButton_click(event) {
+  let user = wixUsers.currentUser;
+  let userId = user.id; // "r5cme-6fem-485j-djre-4844c49"
+  let isLoggedIn = user.loggedIn; // true
+  if (!isLoggedIn) {
+    $w("#followButton").enabled = false;
+  }
+  // let email = await user.getEmail()
   let id = event.context.itemId;
   let messageRow = await wixData
     .query("Messages")
@@ -48,8 +96,6 @@ export async function followButton_click(event) {
   let ownerId = messageRow.items[0]._owner;
   let ownerEmail = messageRow.items[0].Email;
   let may = $w.at(event.context);
-
-  let user = wixUsers.currentUser;
 
   let followRow = await wixData
     .query("following")
@@ -65,7 +111,7 @@ export async function followButton_click(event) {
   }
 }
 
-function follow(followRow, user, ownerEmail) {
+async function follow(followRow, user, ownerEmail) {
   if (!followRow.items.length) {
     let toInsert = {
       _id: user.id,
@@ -83,30 +129,52 @@ function follow(followRow, user, ownerEmail) {
     };
 
     wixData.update("following", toUpdate);
+    await getNotificationData();
   }
 }
 
-function unfollow(followRow, user, OwnerEmail) {
-  if (followRow.items[0].follow.length === 1) {
-    let toEmpty = {
-      _id: user.id,
-      follow: []
-    };
-    wixData.update("following", toEmpty);
+async function unfollow(followRow, user, OwnerEmail) {
+  // if (followRow.items[0].follow.length === 1) {
+  // 	let toEmpty = {
+  // 		_id: user.id,
+  // 		follow: []
+  // 	};
+  // 	wixData.update("following", toEmpty);
+  // } else {
+  let rowBeforeUpdate = followRow.items[0].follow;
+  let idx = rowBeforeUpdate.indexOf(OwnerEmail);
+  let spliced = rowBeforeUpdate.splice(idx, 1);
+  let toUpdate = {
+    _id: user.id,
+    follow: [...spliced]
+  };
+  wixData.update("following", toUpdate);
+  await getNotificationData();
+}
+
+export async function notificationButton_click(event) {
+  showBadge();
+  let isHidden = $w("#notificationsRead").hidden;
+  if (isHidden) {
+    $w("#notificationsRead").show();
+    $w("#notBadge").hide();
   } else {
-    let rowBeforeUpdate = followRow.items[0].follow;
-    let idx = rowBeforeUpdate.indexOf(OwnerEmail);
-    let spliced = rowBeforeUpdate.splice(idx, 1);
-    let toUpdate = {
-      _id: user.id,
-      follow: [...spliced]
-    };
-
-    wixData.update("following", toUpdate);
+    $w("#notificationsRead").hide();
+    $w("#notBadge").hide();
   }
+  let newData = await getNotificationData();
+  $w("#notificationsRead").data = newData;
 }
 
-// let user = wixUsers.currentUser;
-// let userId = user.id; // "r5cme-6fem-485j-djre-4844c49"
-// let isLoggedIn = user.loggedIn; // true
-// let email = await user.getEmail()
+function showBadge() {
+  $w("#notBadge").show();
+}
+
+export function submitButton_click(event) {
+  let user = wixUsers.currentUser;
+  let userId = user.id;
+  let isLoggedIn = user.loggedIn;
+  if (!isLoggedIn) {
+    $w("#submitButton").enabled = false;
+  }
+}
